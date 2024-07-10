557a3432077c0712cf038dc612b14ea440d7d501
stiven <2021110357@udh.edu.pe>
2024-07-10 08:11:53 -0500

    agregando el index

---
+++ index.php
@@ -0,0 +1,7 @@
+</div>
+    <footer class="footer py-3">
+      <p class="w-100 m-0">Derechos Reservados</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
